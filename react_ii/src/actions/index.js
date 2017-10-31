ebcfc41b0350b5e44d6697d12c4fdf7f11fb5512
Jon Sibala <sibalajp@gmail.com>
2017-10-31 10:32:30 -0700

    completed react_ii

--- react_ii/src/actions/index.js
+++ react_ii/src/actions/index.js
@@ -1,3 +1,11 @@
 export function selectBook(book) {
-	console.log('A book has been selected:', book.title);
+	// selectBook is an ActionCreator, it needs to return an action,
+	// an object with a type property
+	return {
+		// l.46 3:40
+		type: 'BOOK_SELECTED',
+		payload: book
+
+	};
+	
 }
--- react_ii/src/containers/book-detail.js
+++ react_ii/src/containers/book-detail.js
@@ -4,8 +4,16 @@ import { connect } from 'react-redux';
 // l.48
 class BookDetail extends Component {
 	render() {
+		if(!this.props.book) {
+			return <div>Select a book to get started.</div>;
+		}
+	
 		return (
-			<div>Book Detail!</div>
+			<div>
+				<h3>Details for:</h3>
+				<div>Title: {this.props.book.title}</div>
+				<div>Pages: {this.props.book.pages}</div>
+			</div>
 		)
 	}
 }
@@ -13,7 +21,7 @@ class BookDetail extends Component {
 
 function mapStateToProps(state) {
 	return {
-		book: state.active
+		book: state.activeBook
 	}
 }
 
